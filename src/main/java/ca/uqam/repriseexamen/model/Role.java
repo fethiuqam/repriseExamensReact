7d6622dce55945f2801f6a10ec04b9cdd566f482
Miquelon, Charles <miquelon.charles.2@courrier.uqam.ca>
2022-06-09 11:15:00 +0000

    TEAM3/CODE_QUALITY - Améliorer et simplifier les vérifications de qualité du code

--- src/main/java/ca/uqam/repriseexamen/model/Role.java
+++ src/main/java/ca/uqam/repriseexamen/model/Role.java
@@ -5,13 +5,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
-import ca.uqam.repriseexamen.model.Permission;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
-
 
 @Entity
 @Data
@@ -21,6 +19,6 @@ public class Role {
     private @Id @GeneratedValue Long id;
     private String name;
 
-    @ElementCollection(targetClass=Permission.class)
+    @ElementCollection(targetClass = Permission.class)
     private List<Permission> permissions;
 }
