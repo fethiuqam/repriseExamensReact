9f8f78fed26ca8fa932e8cd5151b16e338d46d01
Abi Ayad, Fethi Bey <abi_ayad.fethi_bey@courrier.uqam.ca>
2022-06-10 13:10:36 -0400

    mise a jour des tests unitaires de DREexamenController

--- src/test/java/ca/uqam/repriseexamen/controller/DemandeRepriseExamenRestControllerUnitTest.java
+++ src/test/java/ca/uqam/repriseexamen/controller/DemandeRepriseExamenRestControllerUnitTest.java
@@ -1,5 +1,6 @@
 package ca.uqam.repriseexamen.controller;
 
+import ca.uqam.repriseexamen.dto.LigneDRECommisDTO;
 import ca.uqam.repriseexamen.model.*;
 import ca.uqam.repriseexamen.service.DemandeRepriseExamenService;
 import org.junit.Before;
@@ -13,12 +14,14 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Arrays;
@@ -27,12 +30,9 @@ import java.util.List;
 @SpringBootTest
 @RunWith(SpringRunner.class)
 public class DemandeRepriseExamenRestControllerUnitTest {
-
     private MockMvc mockMvc;
-
     @Autowired
     protected WebApplicationContext context;
-
     @MockBean
     private DemandeRepriseExamenService service;
 
@@ -44,39 +44,17 @@ public class DemandeRepriseExamenRestControllerUnitTest {
     @Test
     public void devraitRetournerListeDREavecStatutOk()
             throws Exception {
-        String nom = "Marc";
-        Etudiant etudiant = Etudiant.builder()
-                .nom(nom)
-                .codePermanent("ABCD12345678")
-                .email(nom + "@courrier.uqam.ca")
-                .telephone("1111111111")
-                .build();
-        Justification justification = Justification.builder()
-                .description("justificatif 1")
-                .url("/files/file1")
-                .build();
-        List<Justification> justifications = Arrays.asList(justification);
-        Statut statut = Statut.builder()
-                .typeStatut(TypeStatut.SOUMISE)
-                .dateHeure(LocalDateTime.now())
-                .build();
-        List<Statut> listeStatute = Arrays.asList(statut);
-        DemandeRepriseExamen examRR = DemandeRepriseExamen.builder()
-                .absenceDateDebut(LocalDate.of(2022, 2, 2))
-                .absenceDateFin(LocalDate.of(2022, 2, 10))
-                .etudiant(etudiant)
-                .listeJustification(justifications)
-                .motifAbsence(MotifAbsence.MEDICAL)
-                .listeStatut(listeStatute)
-                .absenceDetails("Intervention chirurgicale programmée")
-                .build();
-        List<DemandeRepriseExamen> examRRList = Arrays.asList(examRR);
-        when(service.getAllDemandeRepriseExamen()).thenReturn(examRRList);
+        List<LigneDRECommisDTO> listeDRECommisDTO = genererListeDRECommisDTOPourTest();
+        when(service.getAllDemandeRepriseExamen()).thenReturn(listeDRECommisDTO);
         this.mockMvc.perform(get("/api/demandes").contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$", hasSize(1)))
-                .andExpect(jsonPath("$[0].motifAbsence", is(examRR.getMotifAbsence().toString())))
-                .andExpect(jsonPath("$[0].detenteur.nom", is(examRR.getEtudiant().getNom())));
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].dateHeureSoumission", is(listeDRECommisDTO.get(0)
+                        .getDateHeureSoumission().toString())))
+                .andExpect(jsonPath("$[0].sigleCours", is(listeDRECommisDTO.get(0).getSigleCours())))
+                .andExpect(jsonPath("$[1].dateHeureSoumission", is(listeDRECommisDTO.get(1)
+                        .getDateHeureSoumission().toString())))
+                .andExpect(jsonPath("$[1].sigleCours", is(listeDRECommisDTO.get(1).getSigleCours())));
     }
 
     @Test
@@ -84,6 +62,37 @@ public class DemandeRepriseExamenRestControllerUnitTest {
             throws Exception {
         this.mockMvc.perform(get("/api/demande").contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isNotFound());
+    }
+
+
+    private List<LigneDRECommisDTO> genererListeDRECommisDTOPourTest() {
+        return Arrays.asList(
+                LigneDRECommisDTO.builder()
+                        .id(1L)
+                        .dateHeureSoumission(LocalDateTime.of(2022, 2, 1, 8, 22, 23))
+                        .statutCourant(TypeStatut.SOUMISE)
+                        .nomEtudiant("Marc Marshall")
+                        .codePermanentEtudiant("AAAA12345678")
+                        .nomEnseignat("Lord Melanie")
+                        .matriculeEnseignat("CCCC12345678")
+                        .sigleCours("INF1120")
+                        .groupe("030")
+                        .session(Session.HIVER)
+                        .build(),
+                LigneDRECommisDTO.builder()
+                        .id(2L)
+                        .dateHeureSoumission(LocalDateTime.of(2021, 1, 31, 8, 22, 23))
+                        .statutCourant(TypeStatut.ACCEPTEE)
+                        .nomEtudiant("Jack Morisson")
+                        .codePermanentEtudiant("BBBB12345678")
+                        .nomEnseignat("Lord Melanie")
+                        .matriculeEnseignat("CCCC12345678")
+                        .sigleCours("INF2120")
+                        .groupe("030")
+                        .session(Session.AUTOMNE)
+                        .build()
+        );
+
     }
 
 }
