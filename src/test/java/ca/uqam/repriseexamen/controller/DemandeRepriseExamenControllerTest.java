462bc27e7b22139ac1426e668399c84744361dda
Abi Ayad, Fethi Bey <abi_ayad.fethi_bey@courrier.uqam.ca>
2022-07-13 11:26:30 -0400

    fix tests demandeController apres merge

--- src/test/java/ca/uqam/repriseexamen/controller/DemandeRepriseExamenControllerTest.java
+++ src/test/java/ca/uqam/repriseexamen/controller/DemandeRepriseExamenControllerTest.java
@@ -35,6 +35,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@Transactional
 @SpringBootTest
 @RunWith(SpringRunner.class)
 @ActiveProfiles("datatest")
