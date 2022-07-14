9e3c9a30b0b4153a5c200d7f0f5ca8e82d941c8d
Quirion, Tristan <quirion.tristan@courrier.uqam.ca>
2022-07-14 00:41:21 +0000

    Team3-Enlever restriction endpoint rest Utilisateurs et Roles

--- src/main/java/ca/uqam/repriseexamen/securite/ConfigurationSecurite.java
+++ src/main/java/ca/uqam/repriseexamen/securite/ConfigurationSecurite.java
@@ -5,7 +5,6 @@ import ca.uqam.repriseexamen.securite.jwt.JWTTokenHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -53,17 +52,6 @@ public class ConfigurationSecurite extends WebSecurityConfigurerAdapter {
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().exceptionHandling()
                 .authenticationEntryPoint(authenticationEntryPoint).and()
                 .authorizeRequests((request) ->request.antMatchers("/api/login/**").permitAll()
-                        //Roles
-                        .antMatchers(HttpMethod.GET,"/api/roles/**").hasAuthority("GererRoles")
-                        .antMatchers(HttpMethod.POST,"/api/roles/**").hasAuthority("GererRoles")
-                        .antMatchers(HttpMethod.PUT,"/api/roles/**").hasAuthority("GererRoles")
-                        .antMatchers(HttpMethod.PATCH,"/api/roles/**").hasAuthority("GererRoles")
-                        //Utilisateurs
-                        .antMatchers(HttpMethod.GET,"/api/utilisateurs/**").hasAuthority("GererUsagers")
-                        .antMatchers(HttpMethod.POST,"/api/utilisateurs/**").hasAuthority("GererUsagers")
-                        .antMatchers(HttpMethod.PUT,"/api/utilisateurs/**").hasAuthority("GererUsagers")
-                        .antMatchers(HttpMethod.PATCH,"/api/utilisateurs/**").hasAuthority("GererUsagers")
-                        //DEFAULT
                         .antMatchers("/api/**").authenticated()
                         .anyRequest().permitAll())
                 .addFilterBefore(new JWTAuthenticationFilter(utilisateurService, jWTTokenHelper),
