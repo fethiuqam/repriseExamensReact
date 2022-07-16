afce9a795e3fb387cdc8e18ba4f217ddf0884746
Miquelon, Charles <miquelon.charles.2@courrier.uqam.ca>
2022-07-16 18:59:52 +0000

    TEAM3 - Retourner la liste des permissions de l'utilisateur lors du login

--- src/main/java/ca/uqam/repriseexamen/model/Utilisateur.java
+++ src/main/java/ca/uqam/repriseexamen/model/Utilisateur.java
@@ -1,18 +1,19 @@
 package ca.uqam.repriseexamen.model;
 
-
-import javax.persistence.*;
-
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.util.Collection;
-
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Entity
 @Data
 @NoArgsConstructor
 @Inheritance
+@AllArgsConstructor
 public abstract class Utilisateur {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,16 +21,14 @@ public abstract class Utilisateur {
     protected String nom;
     protected String prenom;
     protected String email;
-    @Column(unique=true)
+    @Column(unique = true)
     protected String codeMs;
     protected String motDePasse;
     @Column(name = "dtype", insertable = false, updatable = false)
     protected String type;
 
     @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-    @JoinTable(name = "utilisateurs_roles",
-            joinColumns = @JoinColumn(name = "utilisateurs_id"),
-            inverseJoinColumns = @JoinColumn(name = "roles_id"))
+    @JoinTable(name = "utilisateurs_roles", joinColumns = @JoinColumn(name = "utilisateurs_id"), inverseJoinColumns = @JoinColumn(name = "roles_id"))
     protected Collection<Role> roles;
 
     public Utilisateur(String nom, String prenom, String codeMs, String motDePasse, Collection<Role> roles) {
@@ -38,6 +37,12 @@ public abstract class Utilisateur {
         this.codeMs = codeMs;
         this.motDePasse = motDePasse;
         this.roles = roles;
+    }
+
+    public Set<Permission> getPermissions() {
+        return roles.stream()
+                .flatMap(role -> role.getPermissions().stream())
+                .collect(Collectors.toSet());
     }
 
 }
