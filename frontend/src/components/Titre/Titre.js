97ffa0d31c1d678417df043b6bf3f5aaf3151382
Gabrielle Poitras <poitras.gabrielle.2@courrier.uqam.ca>
2022-07-18 12:58:38 +0000

    Team2 frontend ameliorations visuelles

--- frontend/src/components/Titre/Titre.js
+++ frontend/src/components/Titre/Titre.js
@@ -1,12 +1,12 @@
-import { Container, Typography } from '@material-ui/core';
+import { Typography } from '@material-ui/core';
 import * as React from 'react';
 import '../../styles/StyleEtudiant.css'
 
 const Titre = (props) => {
     return (
-        <Container>
+        <div className="divTitre">
             <Typography variant='h4' className='titre'>{props.titre}</Typography>
-        </Container>
+        </div>
     )
 }
 
