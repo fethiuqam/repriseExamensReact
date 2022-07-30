06b5649b2e149be739407f0c2dcfe8d191a9aa8d
Gabrielle Poitras <poitras.gabrielle.2@courrier.uqam.ca>
2022-07-30 19:54:57 +0000

    Team2 - Annuler DRE déjà soumise - Frontend

--- frontend/src/components/DetailsDRE/DetailsDRE.js
+++ frontend/src/components/DetailsDRE/DetailsDRE.js
@@ -5,7 +5,18 @@ import {Link, useParams} from "react-router-dom";
 import MiseEnPage from "../MiseEnPage/MiseEnPage";
 import SectionFormulaire from '../SectionFormulaire/SectionFormulaire';
 import Box from "@mui/material/Box";
-import {Button, CircularProgress, Container, Stack} from "@mui/material";
+import {
+    Button, 
+    CircularProgress, 
+    Container, 
+    Stack, 
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle,
+    Snackbar, 
+    Alert } from "@mui/material";
 import JugerDRE from "../JugerDRE/JugerDRE";
 import TableInfosEnseignant from '../TableInfoEnseignant/TableInforEnseignant';
 import TableDetailsAbsence from '../TableDetailsAbsence/TableDetailsAbsence';
@@ -14,6 +25,7 @@ import TableInfosDemande from '../TableInfosDemande/TableInfosDemande';
 import KeyboardReturnIcon from "@mui/icons-material/KeyboardReturn";
 import MessagesDRE from "../MessagesDRE/MessagesDRE";
 import HistoriqueEtudiant from "../HistoriqueEtudiant/HistoriqueEtudiant";
+import * as apiClient from "../../api/ApiClient";
 
 export default function DetailsDRE() {
 
@@ -22,6 +34,48 @@ export default function DetailsDRE() {
     const [dre, setDre] = useState(null);
     const {idDRE} = useParams();
     let juge = null;
+
+    const [confirmationDialogOpen, setConfirmationDialogOpen] = useState(false);
+    const [confirmationDialogTitre, setConfirmationDialogTitre] = useState("");
+    const [confirmationDialogContenu, setConfirmationDialogContenu] = useState("");
+    const [snackBarOpen, setSnackBarOpen] = useState(false);
+    const [snackBarSeverity, setSnackBarSeverity] = useState("success");
+    const [snackBarTexte, setSnackBarTexte] = useState("");
+    const [snackBarTexteSucces, setSnackBarTexteSucces] = useState("");
+    const [snackBarTexteError, setSnackBarTexteError] = useState("");
+
+    const handleAnnulerDRE = () => {
+        setConfirmationDialogOpen(true);
+        setConfirmationDialogTitre("Confirmer l'annulation de la demande");
+        setConfirmationDialogContenu("Êtes vous sûr de vouloir annuler la demande?");
+        setSnackBarTexteSucces("La demande a été annulée avec succès.");
+        setSnackBarTexteError("Un problème est survenu lors de l'annulation de la demande. Veuillez réessayer.")
+    }
+
+    const annulerDemande = () => {
+        setSnackBarOpen(true);
+        const API_URL = `/api/demandes/${idDRE}/annuler-etudiant`;
+        const patch = {idDRE};
+        const annulerDRE = async () => {
+            try {
+                await apiClient.patch(API_URL, patch);
+                setSnackBarSeverity("success");
+                setSnackBarTexte(snackBarTexteSucces);
+                setSnackBarOpen(true);
+                checherDRE();
+            } catch (err) {
+                setSnackBarSeverity("error");
+                setSnackBarTexte(
+                    err.cause === 406
+                        ? "Cette action n'est pas permise."
+                        : snackBarTexteError
+                );
+                setSnackBarOpen(true);
+            }
+        }
+        annulerDRE();
+        setConfirmationDialogOpen(false);
+    }
 
     switch (type) {
         case "enseignant" :
@@ -122,18 +176,58 @@ export default function DetailsDRE() {
                                 </Button>
 
                                 <Stack direction="row" spacing={1}>
-                                    {juge
-                                        ? <JugerDRE
-                                            idDRE={idDRE}
-                                            juge={juge}
-                                            decisionCourante={dre.decisionCourante.typeDecision}
-                                            statutCourant={dre.statutCourant}
-                                            actualiserDRE={checherDRE}
-                                        />
-                                        : null
-                                    }
-                                </Stack>
+                                    <Button
+                                        onClick={handleAnnulerDRE}
+                                        variant="contained"
+                                        color="error"
+                                        disabled={dre.statutCourant === "ANNULEE"}
+                                    > Annuler la demande
+                                    </Button>
 
+                                    <Dialog
+                                        open={confirmationDialogOpen}
+                                        onClose={() => setConfirmationDialogOpen(false)}
+                                        fullWidth
+                                    >
+                                        <DialogTitle>{confirmationDialogTitre}</DialogTitle>
+                                        <DialogContent>
+                                            <DialogContentText>{confirmationDialogContenu}</DialogContentText>
+                                        </DialogContent>
+                                        <DialogActions>
+                                            <Button autoFocus onClick={() => setConfirmationDialogOpen(false)}>
+                                                Annuler
+                                            </Button>
+                                            <Button onClick={annulerDemande}>Oui, je confirme</Button>
+                                        </DialogActions>
+                                    </Dialog>
+
+                                    <Snackbar
+                                        style={{width: "50%"}}
+                                        anchorOrigin={{vertical: "top", horizontal: "center"}}
+                                        open={snackBarOpen}
+                                        autoHideDuration={5000}
+                                        onClose={() => setSnackBarOpen(false)}
+                                    >
+                                        <Alert
+                                            onClose={() => setSnackBarOpen(false)}
+                                            severity={snackBarSeverity} sx={{width: '100%'}}
+                                        >
+                                            {snackBarTexte}
+                                        </Alert>
+                                    </Snackbar>
+
+                                    <Stack direction="row" spacing={1}>
+                                        {juge &&
+                                            <JugerDRE
+                                                idDRE={idDRE}
+                                                juge={juge}
+                                                decisionCourante={dre.decisionCourante.typeDecision}
+                                                statutCourant={dre.statutCourant}
+                                                actualiserDRE={checherDRE}
+                                            />
+                                        }
+                                    </Stack>
+                                </Stack>
                             </Stack>
                         </div>
                     </Container>
